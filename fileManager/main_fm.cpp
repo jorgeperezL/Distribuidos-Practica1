f6094eaeb41377ef445a5b89fba4d80256e97bc2
mikeherrero21 <58515139+mikeherrero21@users.noreply.github.com>
2021-10-15 19:45:35 +0200

    Update main_fm.cpp

--- fileManager/main_fm.cpp
+++ fileManager/main_fm.cpp
@@ -13,18 +13,23 @@ int main(int argc,char** argv)
     
     
     std::vector<std::string*>* vfiles=fm->listFiles();
-    std::cout<<"Lista de ficheros en el directorio de prueba:\n";
+    std::cout<<"Lista de ficheros en el directorio de prueba:\n"<<std::endl;
     for(unsigned int i=0;i<vfiles->size();++i)
     {
         std::cout<<"Fichero: "<<vfiles->at(i)->c_str()<<std::endl;
     }
-    std::cout<<"Leyendo el primer fichero del directorio de prueba:\n";
+    
 
     char* data=nullptr;
     int fileLen=0;
-    fm->readFile(&(*(vfiles->at(0)))[0],data,fileLen);
-    std::cout<<"Escribiendo el primer fichero del directorio de prueba:\n";
-    fm->writeFile(&(*(vfiles->at(0)))[0],data,fileLen);
+    
+    for(unsigned int i=0;i<vfiles->size();++i){
+    	    std::cout<<"Leyendo el primer fichero del directorio de prueba:"<<std::endl;
+	    fm->readFile(&(*(vfiles->at(i)))[0],data,fileLen);
+	    std::cout<<"Escribiendo el primer fichero del directorio de prueba:\n"<<std::endl;
+	    fm->writeFile(&(*(vfiles->at(i)))[0],data,fileLen);
+	   
+    }
     std::cout<<"Liberando lista de ficheros:\n";
     fm->freeListedFiles(vfiles);
     std::cout<<"Liberando datos de fichero leído:\n";
